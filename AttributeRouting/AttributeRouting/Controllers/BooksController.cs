9d579a5624a364f581501a6ff7099cec5e53c85f
swl <eminthwu@gmail.com>
2017-11-10 21:59:16 +0800

    1st version

---
+++ AttributeRouting/AttributeRouting/Controllers/BooksController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AttributeRouting.Models;
+
+namespace AttributeRouting.Controllers
+{
+    [RoutePrefix("books")]
+    public class BooksController : ApiController
+    {
+        private BookAPIContext db = new BookAPIContext();
+
+        // GET: api/Books
+        [Route("")]
+        public IQueryable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        // GET: api/Books/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> GetBook(int id)
+        {
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Books/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutBook(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.BookId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("")]
+        [Route("{genre}")]
+        [Route("{*genre:regex(\\S*/\\S*)}")]
+        //[Route("{*genre::regex(\\S}")]  // new
+        public IHttpActionResult GetBookByGenre(string genre)
+        {
+            var books = db.Books.Include(b => b.Author)
+                .Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase));
+
+            return Ok(books);
+        }        
+
+
+        [Route("~/authors/{authorId:int}/books")]
+        public IHttpActionResult GetBooksByAuthor(int authorId)
+        {
+            var author = db.Books.Include(b => b.Author)
+                .Where(b => b.AuthorId == authorId);
+
+            return Ok(author);
+        }
+
+        [Route("date/{pubdate:datetime:regex(\\d{4}-\\d{2}-\\d{2})}")]
+        [Route("date/{*pubdate:datetime:regex(\\d{4}/\\d{2}/\\d{2})}")]		// format: yyyy/mm/dd
+        [Route("api/books/date/{pubdate:datetime:regex(\\d{4}-\\d{2}-\\d{2})}")]
+        [Route("date/{pubdate:datetime}")]
+        public IHttpActionResult Get(DateTime pubdate)
+        {
+            var books = db.Books.Include(b => b.Author)
+                .Where(b => DbFunctions.TruncateTime(b.PublishDate)
+                         == DbFunctions.TruncateTime(pubdate));
+
+            return Ok(books);
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        // POST: api/Books
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> PostBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Books.Add(book);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = book.BookId }, book);
+        }
+
+        // DELETE: api/Books/5
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> DeleteBook(int id)
+        {
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(book);
+            await db.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookExists(int id)
+        {
+            return db.Books.Count(e => e.BookId == id) > 0;
+        }
+    }
+}
---
+++ Homework.md
@@ -0,0 +1,43 @@
+﻿# Homework
+
+### 屬性路由
+
+> 注意，"Lab:屬性路由"的請求，拿到空字串或無回傳值就不算完成。每個請求應該有正確的回傳值。
+
+1. 請想辦法正確存取此資源：
+
+```
+localhost:{port}/books/經濟學
+```
+
+不正常：
+
+- Fiddler
+- Postman for Windows
+
+正常：
+
+- Postman for Chrome
+- Microsoft Edge
+- IE 11 (Windows 10)
+- Chrome
+- Firefox
+
+![request-1](request-1.PNG)
+
+左邊是 Chrome 版 Postman 請求正常。
+右邊是 Windows 版 Postman 請求不正常，請想辦法找出不正常的原因。
+
+2. 請想辦法正確存取此資源：
+
+```
+localhost:{port}/books/演講/簡報
+```
+
+成功範例畫面：
+
+![request-2](request-2.png)
+
+### 完成實作
+
+- Lab-Model-Dapper.md
